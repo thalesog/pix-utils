5537a73f550fbcdcb3e5244d5ef23f0fe1ad57ac
Thales Ogliari <thalesog@me.com>
2021-09-26 01:14:33 -0300

    test: extractor tests improved, directory changed

---
+++ src/__tests__/crc.test.ts
@@ -0,0 +1,18 @@
+import { computeCRC } from '../lib-rewrite/crc';
+
+import { DYNAMIC_TEST_EMV, STATIC_TEST_EMV } from './parser.test';
+
+describe('CRC Calculator and Parser', () => {
+  it('should be able to calculate crc', () => {
+    const calculatedCrc = computeCRC(STATIC_TEST_EMV);
+
+    expect(calculatedCrc).toEqual('E5AB');
+  });
+
+  it('should be able to replace wrong crc calculation', () => {
+    const brCodeWithWrongCRC = DYNAMIC_TEST_EMV.replace('C7D7', 'ABCD');
+    const calculatedCrc = computeCRC(brCodeWithWrongCRC);
+
+    expect(calculatedCrc).toEqual('C7D7');
+  });
+});
---
+++ src/__tests__/extractor.test.ts
@@ -0,0 +1,38 @@
+import { parsePix } from '../lib-rewrite/main';
+import {
+  DynamicPixEmvElements,
+  PixElementType,
+  StaticPixEmvElements,
+} from '../lib-rewrite/types/pixElements';
+
+import { DYNAMIC_TEST_EMV, STATIC_TEST_EMV } from './parser.test';
+
+describe('EMV Data Extractor', () => {
+  it('should be able to extract basic elements from a static pix', () => {
+    const parsedPix = parsePix(STATIC_TEST_EMV) as StaticPixEmvElements;
+
+    // PARSE MAI
+    expect(parsedPix.type).toBe(PixElementType.STATIC);
+    expect(parsedPix.merchantCategoryCode).toBe('0000');
+    expect(parsedPix.transactionCurrency).toBe('986');
+    expect(parsedPix.countryCode).toBe('BR');
+    expect(parsedPix.merchantName).toBe('Thales Ogliari');
+    expect(parsedPix.merchantCity).toBe('SC');
+    expect(parsedPix.pixKey).toBe('thalesog@me.com');
+  });
+
+  it('should be able to extract basic elements from a dynamic pix', () => {
+    const parsedPix = parsePix(DYNAMIC_TEST_EMV) as DynamicPixEmvElements;
+
+    // PARSE MAI
+    expect(parsedPix.type).toBe(PixElementType.DYNAMIC);
+    expect(parsedPix.merchantCategoryCode).toBe('0000');
+    expect(parsedPix.transactionCurrency).toBe('986');
+    expect(parsedPix.countryCode).toBe('BR');
+    expect(parsedPix.merchantName).toBe('Pix');
+    expect(parsedPix.merchantCity).toBe('BRASILIA');
+    expect(parsedPix.url).toBe(
+      'qr-h.sandbox.pix.bcb.gov.br/rest/api/v2/cfe8166acaff4a62a18b7b766ef57c70'
+    );
+  });
+});
