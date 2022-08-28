14707708bcf7cb2cdca1e24a2f890c6c1ecb435f
Thales Ogliari <thalesog@me.com>
2022-08-28 16:27:36 -0300

    fix: undefined throwIfError when object is ok (#7)

--- src/assembler.ts
+++ src/assembler.ts
@@ -11,21 +11,41 @@ import { toBase64 } from './utils/qrcodeGenerator';
 
 export function generatePixObject(elements: PixElements): PixObject {
   const emvCode = createEmv(elements);
-  switch (elements.type) {
-    case PixElementType.STATIC:
-      return {
-        ...elements,
-        toBRCode: () => emvCode,
-        toImage: () => toBase64(emvCode),
-      } as PixStaticObject;
+  if (elements.type === PixElementType.STATIC) {
+    const staticObject = {
+      ...elements,
+      toBRCode: () => emvCode,
+      toImage: () => toBase64(emvCode),
+    };
 
-    case PixElementType.DYNAMIC:
-      return {
-        ...elements,
-        toBRCode: () => emvCode,
-        fetchPayload: ({ DPP, codMun }) =>
-          fetchPayload({ url: elements.url, DPP, codMun }),
-        toImage: () => toBase64(emvCode),
-      } as PixDynamicObject;
+    const throwIfError = () => ({
+      ...staticObject,
+      throwIfError: () => staticObject,
+    });
+
+    return {
+      ...staticObject,
+      throwIfError,
+    } as PixStaticObject;
   }
+  if (elements.type === PixElementType.DYNAMIC) {
+    const dynamicObject = {
+      ...elements,
+      toBRCode: () => emvCode,
+      fetchPayload: ({ DPP, codMun }) =>
+        fetchPayload({ url: elements.url, DPP, codMun }),
+      toImage: () => toBase64(emvCode),
+    };
+
+    const throwIfError = () => ({
+      ...dynamicObject,
+      throwIfError: () => dynamicObject,
+    });
+
+    return {
+      ...dynamicObject,
+      throwIfError,
+    } as PixDynamicObject;
+  }
+  throw new Error('Invalid Pix type');
 }
--- tests/throwIfError.test.ts
+++ tests/throwIfError.test.ts
@@ -2,11 +2,28 @@ import { describe, expect, it } from 'vitest';
 import { createStaticPix } from '../src';
 
 describe('throwIfError', () => {
+  it('should be able to return the correct object when no errors are detected', () => {
+    const staticPixFn = createStaticPix({
+      merchantName: 'Thales Ogliari',
+      merchantCity: 'SAO MIGUEL',
+      pixKey: 'nubank@thalesog.com',
+      infoAdicional: 'Pedido 123',
+      txid: '',
+      transactionAmount: 10,
+    }).throwIfError();
+
+    expect(staticPixFn).toEqual(
+      expect.objectContaining({
+        ...staticPixFn,
+      })
+    );
+  });
+
   it('should throw if merchant city is longer than 15 characters', () => {
     const staticPixFn = createStaticPix({
       merchantName: 'Thales Ogliari',
       merchantCity: 'SAO MIGUEL DO OESTE',
-      pixKey: 'thalesog@me.com',
+      pixKey: 'nubank@thalesog.com',
       infoAdicional: 'Pedido 123',
       txid: '',
       transactionAmount: 10,
@@ -21,7 +38,7 @@ describe('throwIfError', () => {
     const staticPixFn = createStaticPix({
       merchantName: 'Thales Ogliari Thales Ogliari Thales Ogliari',
       merchantCity: 'SAO MIGUEL',
-      pixKey: 'thalesog@me.com',
+      pixKey: 'nubank@thalesog.com',
       infoAdicional: 'Pedido 123',
       txid: '',
       transactionAmount: 10,
@@ -36,7 +53,7 @@ describe('throwIfError', () => {
     const staticPixFn = createStaticPix({
       merchantName: 'Thales Ogliari',
       merchantCity: 'SAO MIGUEL',
-      pixKey: 'thalesog@me.com',
+      pixKey: 'nubank@thalesog.com',
       infoAdicional: 'Pedido 123',
       txid: 'F2B8073B0A52461997F53FB2A85FE7E8',
       transactionAmount: 10,
